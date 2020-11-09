fd7bc674c93570512e137cfa0ae962f9114376ea
wdfo7765 <58469883+wdfo7765@users.noreply.github.com>
2020-11-09 18:54:52 +0900

    Unit test code 작성 및 주석 작성

--- FtpDownloader.cs
+++ FtpDownloader.cs
@@ -11,16 +11,15 @@ namespace CodingTestForGT2Junior
     public class FtpDownloader : IDownloader
     {
 
-        List<string> downLoadList = new List<string>();
         string[] downloadFiles;
-        string address, id, password, remotePath, localFolderPath;
+        string address, id, password, remotePath, localFolderPath; //변수선언
 
         StringBuilder result = new StringBuilder();
         FtpWebRequest reqFTP;
 
         bool IDownloader.DoDownload(out string resultMsg)
         {
-            
+            //FTP정보를 입력받음
             Console.WriteLine("<FTP Data Downloader>");
             Console.Write("Input Addrrss to download data: ");
             address = Console.ReadLine();
@@ -35,30 +34,35 @@ namespace CodingTestForGT2Junior
             localFolderPath = Console.ReadLine();
             Console.WriteLine();
 
+            //입력한 정보가 파일인지 폴더인지 구분
             string path = address + remotePath;
             string[] check = path.Split('/');
-            if (check[check.Length-1].IndexOf('.') == -1)
+
+            //입력한 정보가 폴더일 경우
+            if (check[check.Length-1].IndexOf('.') == -1) 
             {
                 string folderPath = "C:"+localFolderPath+"/"+check[check.Length-1];
                 DirectoryInfo di = new DirectoryInfo(folderPath);
-                Console.WriteLine(folderPath);
+                //입력한 폴더가 LocalFolder 존재하지 않을경우 생성
                 if(di.Exists==false)
                 {
                     di.Create();
-                    Console.Write("여기되고있니?");
                 }
-        
-
-
-                //폴더일때
+ 
+                //다운로드 할 파일들의 목록
                 downloadFiles = findList(address, remotePath);
             }
+
+            //입력한 정보가 파일일 경우
             else
             {
+                //파일인 경우 파일이 존재하는 폴더에 있는 파일들을 다운로드
                 address.Substring(0, address.Length - check[check.Length - 1].Length);
-                Console.WriteLine(address);
+                
                 downloadFiles = findList(address, "");
             }
+
+            //파일 목록을 이용하여 다운로드
             for (int i = 0; i < downloadFiles.Length; i++)
             {
                 doDownLoad(address, id, password, localFolderPath, downloadFiles[i]);
@@ -68,17 +72,26 @@ namespace CodingTestForGT2Junior
             return false;
         }
 
-        string[] findList(string address,string remotePath)
+        /// <summary>
+        /// FTP서버에 존재하는 파일 목록을 불러옴
+        /// </summary>
+        /// <param name="address"></param>
+        /// <param name="remotePath"></param>
+        /// <returns></returns>
+        public string[] findList(string address,string remotePath)
         {
             string[] files;
-            Console.WriteLine(address + remotePath );
+
+            //FTP정보들로 FTP서버와 연결
             reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri(address  + remotePath));
             reqFTP.UseBinary = true;
             reqFTP.Credentials = new NetworkCredential(id, password);
             reqFTP.Method = WebRequestMethods.Ftp.ListDirectory;
-            WebResponse response = reqFTP.GetResponse();
+            WebResponse response = reqFTP.GetResponse(); //Response
             StreamReader reader = new StreamReader(response.GetResponseStream());
 
+
+            //Stream을 line으로 저장
             string line = reader.ReadLine();
             while (line != null)
             {
@@ -89,20 +102,33 @@ namespace CodingTestForGT2Junior
             result.Remove(result.ToString().LastIndexOf('\n'), 1);
             reader.Close();
             response.Close();
-
+            //files에 Stream으로 받은 정보를 저장
             files = result.ToString().Split('\n');
             for (int i = 0; i < files.Length; i++)
             {
-                Console.WriteLine(files[i] + "이건가");
+                //다운받을 파일 목록을 출력
+                Console.WriteLine(files[i]);
             }
             return files;
         }
-        void doDownLoad(string address, string id, string password,string localFolderPath, string downloadFile)
+
+        /// <summary>
+        /// FTP서버에 존재하는 파일들을 다운로드
+        /// </summary>
+        /// <param name="address"></param>
+        /// <param name="id"></param>
+        /// <param name="password"></param>
+        /// <param name="localFolderPath"></param>
+        /// <param name="downloadFile"></param>
+        public void doDownLoad(string address, string id, string password,string localFolderPath, string downloadFile)
         {
+            //다운로드 할 목록이 파일이 아닌경우 종료
             if(downloadFile.IndexOf('.') ==-1)
             {
                 return;
             }
+
+            //FTP 정보들로 서버와 연결
             Uri sourceFileUri = new Uri(address + downloadFile);
             FtpWebRequest ftpWebRequest = (FtpWebRequest)WebRequest.Create(sourceFileUri);
 
@@ -114,19 +140,19 @@ namespace CodingTestForGT2Junior
 
             FileStream targetFileStream = new FileStream(localFolderPath + "/" + downloadFile, FileMode.Create, FileAccess.Write);
 
-
+            //최대 길이
             int length = 2048;
             Byte[] buffer = new Byte[length];
             int bytesRead = sourceStream.Read(buffer, 0, length);
             while (bytesRead > 0)
             {
+                //Stream으로 받아온 정보를 파일에 Write
                 targetFileStream.Write(buffer, 0, length);
                 bytesRead = sourceStream.Read(buffer, 0, length);
             }
             targetFileStream.Close();
             sourceStream.Close();
 
-            downLoadList.Add(downloadFile);
         }
     }
 }
