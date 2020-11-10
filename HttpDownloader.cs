c635d4b8369a7c5fc534b4b9f75769930afaae17
wdfo7765 <58469883+wdfo7765@users.noreply.github.com>
2020-11-10 18:19:03 +0900

    http 다운로더 리팩토링(진행중)

--- HttpDownloader.cs
+++ HttpDownloader.cs
@@ -10,26 +10,24 @@ namespace CodingTestForGT2Junior
 {
     public class HttpDownloader : IDownloader
     {
+    
+        /// <summary>
+        /// Http 다운로드 함수
+        /// </summary>
+        /// <param name="resultMsg"></param>
+        /// <returns></returns>
         public bool DoDownload(out string resultMsg)
         {
+            //변수 선언
             string url, downlaodLocalFolderPath;
 
-            Console.WriteLine();
-            Console.WriteLine("<HTTP Data Downloader>");
-            Console.Write("Input URL to download data: ");
-            url = Console.ReadLine();
-            Console.WriteLine();
-            Console.Write("Input target file path: ");
-            downlaodLocalFolderPath = Console.ReadLine();
-            Console.WriteLine();
+            //프린트 출력 및 입력 함수
+            string[] inputInfo = printHelp();
+            url = inputInfo[0];
+            downlaodLocalFolderPath = inputInfo[1];
 
-
-            if (File.Exists(downlaodLocalFolderPath))
-            {
-                FileInfo file = new FileInfo(downlaodLocalFolderPath);
-                file.IsReadOnly = false;
-                File.Delete(downlaodLocalFolderPath);
-            }
+            //파일이 존재하는지 확인
+            isExist(downlaodLocalFolderPath);
 
 
             if(!Directory.CreateDirectory(Path.GetDirectoryName(downlaodLocalFolderPath)).Exists)
@@ -41,12 +39,12 @@ namespace CodingTestForGT2Junior
             //http://www.celestrak.com/NORAD/elements/tle-new.txt
             using (var client = new WebClient())
             {
-    
                 client.DownloadFile(url, downlaodLocalFolderPath);
+            }
 
-               }
 
-            if (File.Exists(downlaodLocalFolderPath))
+            //다운로드가 되었는지 확인  -------- 이함수 별로필요없는거같음 확인 필요
+            if (isDownloaded(downlaodLocalFolderPath))
             {
                 resultMsg = "Succeeded to download data from URL.";
                 return true;
@@ -54,6 +52,53 @@ namespace CodingTestForGT2Junior
             else
             {
                 resultMsg = "Failed to download data form URL.";
+                return false;
+            }
+        }
+        /// <summary>
+        /// 안내문 출력 및 정보 입력받기
+        /// </summary>
+        public string[] printHelp()
+        {
+            string url, downlaodLocalFolderPath;
+            Console.WriteLine();
+            Console.WriteLine("<HTTP Data Downloader>");
+            Console.Write("Input URL to download data: ");
+            url = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Input target file path: ");
+            downlaodLocalFolderPath = Console.ReadLine();
+            Console.WriteLine();
+
+            string[] info = { url, downlaodLocalFolderPath };
+            return info;
+        }
+        /// <summary>
+        /// 파일이 존재하는지 확인, 존재한다면 삭제
+        /// </summary>
+        public void isExist(string downlaodLocalFolderPath)
+        {
+            if (File.Exists(downlaodLocalFolderPath))
+            {
+                FileInfo file = new FileInfo(downlaodLocalFolderPath);
+                file.IsReadOnly = false;
+                File.Delete(downlaodLocalFolderPath);
+            }
+        }
+        /// <summary>
+        /// 다운로드가 되었는지 확인
+        /// </summary>
+        /// <param name="downlaodLocalFolderPath"></param>
+        /// <returns></returns>
+        public bool isDownloaded(string downlaodLocalFolderPath )
+        {
+
+            if (File.Exists(downlaodLocalFolderPath))
+            {
+                return true;
+            }
+            else
+            {
                 return false;
             }
         }
